de465cf02a19dee8f221838a4a4dc6a12291bf47
Sacrimento <sacrimentocorp@gmail.com>
2020-07-18 22:59:42 +0200

    basic process checker

--- server/taskmaster.py
+++ server/taskmaster.py
@@ -5,6 +5,7 @@ import sys
 import signal
 import socket
 import logging
+from datetime import datetime, timedelta
 
 class Taskmaster:
     _processes = {}
@@ -44,13 +45,13 @@ class Taskmaster:
         signal.signal(signal.SIGHUP, lambda _, __: self.update_conf())
         
         self.logger.info('taskmaster daemon started')
-        self.update_conf(False)
+        self.update_conf(False, True)
 
     def run(self):
         while True:
             if self.autoreload and self.has_conf_changed():
                 self.update_conf()
-            # TODO : check_processes() ## Maybe not needed, check SIGCHILD
+            self.check_processes()
 
     def __del__(self):
         if os.path.isfile(self.LOCK):
@@ -72,20 +73,41 @@ class Taskmaster:
             ret = getattr(self, s[0])(*s[1:])
         self.conn.send('OK!'.encode('utf-8'))
 
-    def update_tasks(self, changes):
+    def update_tasks(self, changes, first=False):
         for todo in ('start', 'stop'):
             for task in changes[todo]:
-                if todo != 'start' or self._conf[task].get('autostart', True):
+                if todo != 'start' or (first and self._conf[task].get('autostart', True)):
                     getattr(self, todo)(task)
 
-    def update_conf(self, p=True):
+    def update_conf(self, p=True, first=False):
         conf_changes = self._conf.populate()
         if not conf_changes or (not conf_changes['start'] and not conf_changes['stop']):
             return
         if p:
             self.logger.info('Config file updated !')
             self.logger.debug(self._conf)
-        self.update_tasks(conf_changes)
+        self.update_tasks(conf_changes, first)
+
+    def check_processes(self):
+        ## TODO: WTF is 'stoptime'
+        now = datetime.now()
+
+        for proc_name, status in self._processes.items():
+            conf = self._conf[proc_name]
+            running = not status['process'].poll()
+            if not running:
+                if timedelta(0, conf.get('starttime', 0)) + status['start_date'] < now: # process is running properly
+                    status['status'] = 'running'
+            else:
+                if status['status'] != 'exited':
+                    self.logger.info('%s exited !', proc_name)
+                    status['status'] = 'exited' 
+                if (status['process'].returncode not in conf.get('exitcodes', [])
+                        and conf.get('autorestart', '') == 'unexpected'
+                        and status['retries'] < conf.get('startretries', 2147483647)):
+                    self.start(proc_name)
+                    status['retries'] += 1
+
 
     def has_conf_changed(self):
         return self._conf.has_changed()
@@ -110,7 +132,13 @@ class Taskmaster:
                     env=env,
                     preexec_fn=lambda : self.initchildproc(name))
         self.logger.info('%s started !', name)
-        self._processes[name] = process
+        if name not in self._processes:
+            self._processes[name] = {
+                'retries': 0,
+            }
+        self._processes[name]['process'] = process
+        self._processes[name]['start_date'] = datetime.now()
+        self._processes[name]['status'] = 'started'
 
     def stop(self, name):
         # TODO : check if running
@@ -118,14 +146,15 @@ class Taskmaster:
         if self._handle_bad_name(name):
             return
         try:
-            os.kill(self._processes[name].pid, getattr(signal, "SIG" + self._conf[name].get('stopsignal', "TERM")))
+            os.kill(self._processes[name].pid, getattr(signal, 'SIG' + self._conf[name].get('stopsignal', 'TERM')))
         except OSError:
             return self.logger.error('No pid for process %s', name)
         # os.killpg(os.getpgid(self._processes[name].pid), getattr(signal, self.conf.get('stopsignal', "TERM")))
         self.logger.info('%s stopped !', name)
 
     def status(self):
-        pass
+        for proc_name, status in self._processes:
+            print(proc_name, status)
 
     def _handle_bad_name(self, name):
         if not name:
